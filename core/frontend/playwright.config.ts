64ff44405c3730fa4abdf5935c3e4742add06fbb
RedCrafter07 <gh@r07.dev>
2025-02-09 14:11:08 +0100

    Update config

--- core/frontend/playwright.config.ts
+++ core/frontend/playwright.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'bun run build && bun run preview',
+		port: 4173,
 	},
 
-	testDir: 'e2e'
+	testDir: 'e2e',
 });
