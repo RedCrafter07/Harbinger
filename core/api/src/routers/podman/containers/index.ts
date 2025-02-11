34038725227f03552fd20c34d5c2336ec4c5ac13
RedCrafter07 <gh@r07.dev>
2025-02-11 18:26:55 +0100

    Use p instead of publicProcedure May be helpful if the procedure gets replaced in the future

--- core/api/src/routers/podman/containers/index.ts
+++ core/api/src/routers/podman/containers/index.ts
@@ -7,136 +7,131 @@ import {
 	type ContainerListInput,
 } from '@util/schemas/containers';
 
+const p = publicProcedure;
+
 export const containerRouter = router({
-	create: publicProcedure
-		.input(containerCreateSchema)
-		.mutation(async ({ input }) => {
-			try {
-				const {
-					image,
-					command,
-					entrypoint,
-					env,
-					labels,
-					exposedPorts,
-					hostConfig,
-				} = input;
+	create: p.input(containerCreateSchema).mutation(async ({ input }) => {
+		try {
+			const {
+				image,
+				command,
+				entrypoint,
+				env,
+				labels,
+				exposedPorts,
+				hostConfig,
+			} = input;
 
-				const portBindings =
-					exposedPorts?.reduce((acc, portConfig) => {
-						const { protocol, containerPort, hostPort } =
-							portConfig;
-						const key = `${containerPort}/${protocol}`; // 80/tpc, for example
-						acc[key] = [
-							{
-								HostPort: hostPort
-									? String(hostPort)
-									: undefined,
-							},
-						];
-						return acc;
-					}, {} as { [key: string]: { HostPort?: string }[] }) || {};
+			const portBindings =
+				exposedPorts?.reduce((acc, portConfig) => {
+					const { protocol, containerPort, hostPort } = portConfig;
+					const key = `${containerPort}/${protocol}`; // 80/tpc, for example
+					acc[key] = [
+						{
+							HostPort: hostPort ? String(hostPort) : undefined,
+						},
+					];
+					return acc;
+				}, {} as { [key: string]: { HostPort?: string }[] }) || {};
 
-				const exposedPortsObj =
-					exposedPorts?.reduce((acc, portConfig) => {
-						const { protocol, containerPort } = portConfig;
-						const key = `${containerPort}/${protocol}`;
-						acc[key] = {};
-						return acc;
-					}, {} as { [key: string]: {} }) || {};
+			const exposedPortsObj =
+				exposedPorts?.reduce((acc, portConfig) => {
+					const { protocol, containerPort } = portConfig;
+					const key = `${containerPort}/${protocol}`;
+					acc[key] = {};
+					return acc;
+				}, {} as { [key: string]: {} }) || {};
 
-				const createOptions = {
-					Image: image,
-					Cmd: command,
-					Entrypoint: entrypoint,
-					Env: env
-						? Object.entries(env).map(
-								([key, value]) => `${key}=${value}`,
-						  )
-						: undefined,
-					Labels: labels,
-					ExposedPorts: exposedPortsObj,
-					HostConfig: {
-						NetworkMode: hostConfig?.networkMode,
-						AutoRemove: hostConfig?.autoRemove,
-						Memory: hostConfig?.memory,
-						MemorySwap: hostConfig?.memorySwap,
-						PortBindings: portBindings,
-					},
-				};
+			const createOptions = {
+				Image: image,
+				Cmd: command,
+				Entrypoint: entrypoint,
+				Env: env
+					? Object.entries(env).map(
+							([key, value]) => `${key}=${value}`,
+					  )
+					: undefined,
+				Labels: labels,
+				ExposedPorts: exposedPortsObj,
+				HostConfig: {
+					NetworkMode: hostConfig?.networkMode,
+					AutoRemove: hostConfig?.autoRemove,
+					Memory: hostConfig?.memory,
+					MemorySwap: hostConfig?.memorySwap,
+					PortBindings: portBindings,
+				},
+			};
 
-				Object.keys(createOptions.HostConfig).forEach((key) => {
-					const k = key as keyof (typeof createOptions)['HostConfig'];
-					if (createOptions.HostConfig[k] === undefined) {
-						delete createOptions.HostConfig[k];
-					}
-				});
-
-				const res = await containers.post(
-					'/containers/create',
-					createOptions,
-				);
-
-				switch (res.status) {
-					case 201:
-						return { id: res.data.Id };
-					case 400:
-						throw new TRPCError({
-							code: 'BAD_REQUEST',
-							message: `Error from Socket: ${res.data.message}`,
-							cause: res.data.cause,
-						});
-					case 404:
-						throw new TRPCError({
-							code: 'NOT_FOUND',
-							message: `Error from Socket: ${res.data.message}`,
-							cause: res.data.cause,
-						});
-					case 409:
-						throw new TRPCError({
-							code: 'CONFLICT',
-							message: `Error from Socket: ${res.data.message}`,
-							cause: res.data.cause,
-						});
-					case 500:
-						throw new TRPCError({
-							code: 'INTERNAL_SERVER_ERROR',
-							message: `Error from Socket: ${res.data.message}`,
-						});
+			Object.keys(createOptions.HostConfig).forEach((key) => {
+				const k = key as keyof (typeof createOptions)['HostConfig'];
+				if (createOptions.HostConfig[k] === undefined) {
+					delete createOptions.HostConfig[k];
 				}
-			} catch (error) {
-				console.error('Error creating container:', error);
-				throw new TRPCError({
-					code: 'INTERNAL_SERVER_ERROR',
-					message: 'Failed to create container',
-					cause: error,
-				});
-			}
-		}),
-	list: publicProcedure
-		.input(containerListSchema)
-		.query(async ({ input }) => {
-			try {
-				const { all, size, filters, limit } = input;
+			});
 
-				function handleFilters(filters: ContainerListInput['filters']) {
-					return JSON.stringify(filters);
-				}
+			const res = await containers.post(
+				'/containers/create',
+				createOptions,
+			);
 
-				await containers.get('/containers/json', {
-					query: {
-						all,
-						size,
-						limit,
-						filters: handleFilters(filters),
-					},
-				});
-			} catch (error) {
-				throw new TRPCError({
-					code: 'INTERNAL_SERVER_ERROR',
-					message: 'Could not list containers',
-					cause: error,
-				});
+			switch (res.status) {
+				case 201:
+					return { id: res.data.Id };
+				case 400:
+					throw new TRPCError({
+						code: 'BAD_REQUEST',
+						message: `Error from Socket: ${res.data.message}`,
+						cause: res.data.cause,
+					});
+				case 404:
+					throw new TRPCError({
+						code: 'NOT_FOUND',
+						message: `Error from Socket: ${res.data.message}`,
+						cause: res.data.cause,
+					});
+				case 409:
+					throw new TRPCError({
+						code: 'CONFLICT',
+						message: `Error from Socket: ${res.data.message}`,
+						cause: res.data.cause,
+					});
+				case 500:
+					throw new TRPCError({
+						code: 'INTERNAL_SERVER_ERROR',
+						message: `Error from Socket: ${res.data.message}`,
+					});
 			}
-		}),
+		} catch (error) {
+			console.error('Error creating container:', error);
+			throw new TRPCError({
+				code: 'INTERNAL_SERVER_ERROR',
+				message: 'Failed to create container',
+				cause: error,
+			});
+		}
+	}),
+	list: p.input(containerListSchema).query(async ({ input }) => {
+		try {
+			const { all, size, filters, limit } = input;
+
+			function handleFilters(filters: ContainerListInput['filters']) {
+				return JSON.stringify(filters);
+			}
+
+			await containers.get('/containers/json', {
+				query: {
+					all,
+					size,
+					limit,
+					filters: handleFilters(filters),
+				},
+			});
+		} catch (error) {
+			throw new TRPCError({
+				code: 'INTERNAL_SERVER_ERROR',
+				message: 'Could not list containers',
+				cause: error,
+			});
+		}
+	}),
 });
