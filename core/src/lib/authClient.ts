a1d0bde1dd9e0865d71cfdcfb8e24c1f67d58119
RedCrafter07 <gh@r07.dev>
2025-12-31 14:46:43 +0100

    chore(auth): update authClient and server auth to include passkey and two-factor plugins

--- core/src/lib/authClient.ts
+++ core/src/lib/authClient.ts
@@ -1,6 +1,8 @@
+import { passkeyClient } from '@better-auth/passkey/client';
 import { ssoClient } from '@better-auth/sso/client';
+import { twoFactorClient } from 'better-auth/plugins';
 import { createAuthClient } from 'better-auth/svelte';
 
 export const authClient = createAuthClient({
-	plugins: [ssoClient()]
+	plugins: [passkeyClient(), ssoClient(), twoFactorClient()]
 });
--- core/src/lib/server/auth/index.ts
+++ core/src/lib/server/auth/index.ts
@@ -1,7 +1,9 @@
 import { db } from '$lib/server/db';
+import { passkey } from '@better-auth/passkey';
 import { sso } from '@better-auth/sso';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { twoFactor } from 'better-auth/plugins';
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -10,5 +12,5 @@ export const auth = betterAuth({
 	emailAndPassword: {
 		enabled: true
 	},
-	plugins: [sso()]
+	plugins: [passkey(), sso(), twoFactor()]
 });
